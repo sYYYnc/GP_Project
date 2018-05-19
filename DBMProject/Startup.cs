f34035bc34dbc73da14964555a868fa6781b2a69
sYYYnc <edu__correia@hotmail.com>
2018-05-19 20:06:27 +0100

    Marcação da criação do branch para login e registo atraves de um comentario na class de startup

--- DBMProject/Startup.cs
+++ DBMProject/Startup.cs
@@ -26,6 +26,7 @@ namespace DBMProject
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //Criação de branch para login/registo
             
 
             services.AddDbContext<ApplicationDbContext>(options =>
