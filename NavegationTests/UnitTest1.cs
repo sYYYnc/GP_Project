8293b30dec5b7cac965ba013cd2df47a7e92c4b8
sYYYnc <edu__correia@hotmail.com>
2018-05-28 23:09:21 +0100

    Adição de testes de navegação

--- NavegationTests/UnitTest1.cs
+++ NavegationTests/UnitTest1.cs
@@ -37,13 +37,71 @@ namespace NavegationTests
         {
             IWebDriver driver = new ChromeDriver();
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
-            driver.Navigate().GoToUrl("https://projetog6.azurewebsites.net/");
+            driver.Navigate().GoToUrl("http://it4u.azurewebsites.net/");
             driver.Manage().Window.Maximize();
 
             autenticarUtilizador(driver, "edu__correia@hotmail.com", "123456789");
         }
 
-        
+        /// <summary>
+        /// Método que autentica um utilizador através do objeto do tipo WebDriver.
+        /// </summary>
+        public void autenticarUtilizador(IWebDriver driver, string email, string password)
+        {
+            driver.FindElement(By.XPath("//a[@href='/Account/Login']")).Click();
+            driver.FindElement(By.Id("Email")).SendKeys(email);
+            driver.FindElement(By.Id("Password")).SendKeys(password);
+            driver.FindElement(By.ClassName("btn")).SendKeys(Keys.Enter);
+        }
+
+        /// <summary>
+        /// Método que preenche o form para adicionar um projeto.
+        /// </summary>
+        private void fillFormAddProjeto(IWebDriver driver, string nome, string tec, string des)
+        {
+            driver.FindElement(By.XPath("//a[@href='/Projetos/Create']")).Click();
+            driver.FindElement(By.Id("ProjectName")).SendKeys(nome);
+            driver.FindElement(By.Id("Technology")).SendKeys(tec);
+            driver.FindElement(By.Id("Description")).SendKeys(des);
+        }
+
+        /// <summary>
+        /// Método responsável por testar a página ./Projetos. Página responsável por mostrar
+        /// os projetos disponiveis, adicionar projetos, download e detalhes de cada projeto.
+        /// </summary>
+        [TestMethod]
+        public void testProjetosDisponiveisSLogin()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            driver.Navigate().GoToUrl("http://it4u.azurewebsites.net/");
+            driver.Manage().Window.Maximize();
+
+            driver.FindElement(By.XPath("//a[@href='/Projetos']")).Click();
+
+
+        }
+
+        /// <summary>
+        /// Método responsável por testar a página ./Candidaturas/Create referente à criação
+        /// de uma nova candidatura por parte do utilizador logado.
+        /// </summary>
+        [TestMethod]
+        public void testAdicaoProjeto()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            driver.Navigate().GoToUrl("http://it4u.azurewebsites.net/");
+            driver.Manage().Window.Maximize();
+
+            autenticarUtilizador(driver, "edu__correia@hotmail.com", "123456789");
+            driver.FindElement(By.XPath("//a[@href='/Projetos']")).Click();
+
+            fillFormAddProjeto(driver, "ProjetoTeste", "TecnologiaTeste", "DescriçaoTeste");
+
+        }
+
+        /* 
 
         /// <summary>
         /// Método responsável por testar a página ./Candidaturas/Create referente à criação
@@ -54,11 +112,11 @@ namespace NavegationTests
         {
             IWebDriver driver = new ChromeDriver();
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
-            driver.Navigate().GoToUrl("https://projetog6.azurewebsites.net/");
+            driver.Navigate().GoToUrl("http://it4u.azurewebsites.net/");
             driver.Manage().Window.Maximize();
 
             autenticarUtilizador(driver, "edu__correia@hotmail.com", "123456789");
-            driver.FindElement(By.XPath("//a[@href='/Candidaturas/Create']")).Click();
+            driver.FindElement(By.XPath("//a[@href='/Projetos']")).Click();
 
             IWebElement identifier = driver.FindElement(By.Id("ProgramaDestino_ProgramaMobilidadeID"));
             new SelectElement(driver.FindElement(By.Id("ProgramaDestino_ProgramaMobilidadeID"))).SelectByIndex(1);
@@ -94,18 +152,11 @@ namespace NavegationTests
             driver.Manage().Window.Maximize();
 
             driver.FindElement(By.XPath("//a[@href='/ProgramasMobilidade/Apresentacao']")).Click();
-        }
+        }  
 
-        /// <summary>
-        /// Método que autentica um utilizador através do objeto do tipo WebDriver.
-        /// </summary>
-        public void autenticarUtilizador(IWebDriver driver, string email, string password)
-        {
-            driver.FindElement(By.XPath("//a[@href='/Account/Login']")).Click();
-            driver.FindElement(By.Id("textEmail")).SendKeys(email);
-            driver.FindElement(By.Id("textPassword")).SendKeys(password);
-            driver.FindElement(By.ClassName("btn")).SendKeys(Keys.Enter);
-        }
+    */
+
+        
     }
 }
 
