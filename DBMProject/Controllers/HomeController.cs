7ed1c96f86abbee33c931f8169862d67e1357d71
afcLadeira <32864936+afcLadeira@users.noreply.github.com>
2018-06-04 21:34:13 +0100

    more layout changes
    more layout changes

--- DBMProject/Controllers/HomeController.cs
+++ DBMProject/Controllers/HomeController.cs
@@ -5,11 +5,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using DBMProject.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace DBMProject.Controllers
 {
     public class HomeController : Controller
     {
+
+        [Authorize]
         public IActionResult Index()
         {
             return View();
